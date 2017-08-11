8cd16a8c7539f1d700148d00803179dd9a30b2bc
Dmitry Umbras <freeudv@mail.ru>
2017-08-11 21:33:50 +0300

    Done

---
+++ exercises/exercise-3.js
@@ -0,0 +1,9 @@
+  //Создайте функцию `print`, с локальной переменной `name` и локальной функцией `log`, в которой имеется локальная переменная `output`:
+
+function print() {
+  var name;
+
+  function log() {
+    var output;
+  }
+}
